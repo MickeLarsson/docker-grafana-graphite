1e297007c058f009c6fed0b26dee58fb9bf29208
Micke Larsson <mikael.larsson@laget.se>
2016-03-01 22:43:36 +0100

    Update config.js
    Don't send idle timers or counters

--- statsd/config.js
+++ statsd/config.js
@@ -7,6 +7,9 @@
   graphitePort: 2003,
   graphiteHost: "127.0.0.1",
   flushInterval: 10000,
+  
+  deleteTimers: true,
+  deleteCounters: true,
 
   backends: ['./backends/graphite'],
   graphite: {
